d4d97bdaacf7152b3cb065b8f8799b764140b52e
Samhan097 <48414384+Samhan097@users.noreply.github.com>
2022-03-21 22:58:54 +0530

    connect with backend

--- whatsapp-backend/app.js
+++ whatsapp-backend/app.js
@@ -1,47 +1,16 @@
+// importing
 import express from "express";
-import mongoose from "mongoose";
-import Cors from "cors";
-import Crads from "./dbCrads.js";
 
-// App config
+// app config
 const app = express();
-const port = process.env.PORT || 8001;
-const connection_url =
-  "mongodb+srv://admin:gPw3VBNFtsjLA0zB@cluster0.effr4.mongodb.net/tinderdb?retryWrites=true&w=majority";
+const port = process.env.PORT || 9000;
+// middlewares
 
-// Middlewares
-app.use(express.json());
-app.use(Cors());
+// Do config
 
-// DB config
-mongoose.connect(connection_url, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-// API Endpoints
-app.get("/", (req, res) => res.status(200).send("Hello @!!"));
+// ????
 
-app.post("/tinder/cards", (req, res) => {
-  const dbCard = req.body;
-
-  Crads.create(dbCard, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
-});
-
-app.get("/tinder/cards", (req, res) => {
-  Crads.find((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
-});
-// Listener
-app.listen(port, () => console.log(`listning on localhost port: ${port}`));
+// api routes
+app.get("/", (req, res) => res.status(200).send("Hello world"));
+// listner
+app.listen(port, () => console.log(`Listning on localhost:${port}`));
